66bc7e4dd9f410b8bf862d10865a6baa20b3c43d
rafael <rafaelferreiracosta26@gmail.com>
2020-04-12 22:59:34 -0300

    tank find healer and asking for heal. healer receiving message and replying with heal value

--- src/agents/chars/Healer.java
+++ src/agents/chars/Healer.java
@@ -5,22 +5,63 @@
  */
 package agents.chars;
 
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
+
 /**
  *
  * @author rafae
  */
-public class Healer extends Char{
-
+public class Healer extends Char {
+    
+    private final int HEAL_AMOUT = 150;
+    
     @Override
     protected void setup() {
         Object[] args = getArguments();
-        if(args != null && args.length > 0){
-            setMAX_HP(Integer.parseInt((String)args[0]));
-            setCURRENT_HP(Integer.parseInt((String)args[0]));
-            System.out.println(args[0]);
+        if (args != null && args.length > 0) {
+            setMAX_HP(Integer.parseInt((String) args[0]));
+            setCURRENT_HP(Integer.parseInt((String) args[0]));
         }
-        
+
         System.out.println("Healer " + getAID().getName() + " is ready with " + getCURRENT_HP() + " HP");
+
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("heal");
+        sd.setName("Healing");
+        dfd.addServices(sd);
+
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException ex) {
+            ex.printStackTrace();
+        }
+
+        addBehaviour(new TickerBehaviour(this, 5000) {
+
+            @Override
+            protected void onTick() {
+                ACLMessage msg = myAgent.receive();
+                if (msg != null) {
+                    ACLMessage reply = msg.createReply();
+                    if (msg.getPerformative() == ACLMessage.REQUEST) {
+                        System.out.println(msg.getSender().getName() + " needs healing");
+                        reply.setPerformative(ACLMessage.INFORM);
+                        reply.setContent(Integer.toString(HEAL_AMOUT));
+                        myAgent.send(reply);
+                    } else {
+                        block();
+                    }
+
+                }
+            }
+        });
     }
 
     @Override
@@ -28,5 +69,5 @@ public class Healer extends Char{
         super.takeDown();
         System.out.println("Healer " + getAID().getName() + " leaving");
     }
-    
+
 }
